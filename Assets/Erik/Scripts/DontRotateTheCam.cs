40b7c4d2429e22a16d7c04c14fb951d0153e678a
Erik Torenstam <erik.torenstam@futuregames.nu>
2020-12-01 12:25:40 +0100

    Added a git.ignore

---
+++ Assets/Erik/Scripts/DontRotateTheCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontRotateTheCam : MonoBehaviour
+{
+    [SerializeField] GameObject playerCharacter;
+    [SerializeField] Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       rb.velocity = new Vector3(playerCharacter.transform.position.x - transform.position.x, playerCharacter.transform.position.y - transform.position.y, -10);
+    }
+}
---
+++ Assets/Erik/Scripts/PlayerRotateMoveScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotateMoveScript : MonoBehaviour
+{
+    [Range(0, 10000)] [SerializeField] int MAXIMUMVELOCITY;
+    [Range(0, 10)][SerializeField] float rotationSpeed;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] PhysicsMaterial2D[] mat;
+    bool IsCharging;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) { IsCharging = true; StartCoroutine(AutoStopCharge()); }
+        else if (Input.GetMouseButtonUp(0)) IsCharging = false;
+        if (!IsCharging)
+        {
+            if (rb.angularVelocity < MAXIMUMVELOCITY && rb.angularVelocity > -MAXIMUMVELOCITY)
+                rb.angularVelocity -= Input.GetAxis("Horizontal") * rotationSpeed;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (rb.angularVelocity < 0)
+                    rb.angularVelocity += rotationSpeed * 10;
+                else if (rb.angularVelocity > 0)
+                    rb.angularVelocity -= rotationSpeed * 10;
+
+            }
+        }
+        if (IsCharging) { Charge(); GetComponent<Rigidbody2D>().sharedMaterial = mat[1]; } else GetComponent<Rigidbody2D>().sharedMaterial = mat[0];
+    }
+    private void Charge()
+    {
+        rb.velocity = Vector2.zero;
+    }
+    private IEnumerator AutoStopCharge()
+    {
+        yield return new WaitForSecondsRealtime(1);
+        IsCharging = false;
+    }
+}
