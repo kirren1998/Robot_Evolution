b3b3f0841b9fe91ac2c5e1e6d035efe50bcce9ae
Erik Torenstam <erik.torenstam@futuregames.nu>
2020-12-10 16:42:15 +0100

    Daily Commie (10-12-20)

---
+++ Assets/Pause_Screen_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Screen_Script : MonoBehaviour
+{
+    bool isPaused;
+    [SerializeField] Canvas can;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            if (isPaused)
+            {
+                Time.timeScale = 0;
+                can.enabled = true;
+            } 
+            else
+            {
+                Time.timeScale = 1;
+                can.enabled = false;
+            }
+        }
+    }
+}
