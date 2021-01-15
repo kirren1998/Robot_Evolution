25ddf5e42524cfb00f46edb7fb139adbdb887b4c
0TheMayor0 <eric.strand.oberg97@futuregames.nu>
2021-01-15 13:06:31 +0100

    Main Menu stuff

---
+++ Assets/Eric/Scripts/LightFade.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightFade : MonoBehaviour
+{
+
+    [SerializeField] float fadeSpeed = 0.1f;
+    Light2D global;
+    public bool fadingPlay = false;
+    public bool fadingQuit = false;
+
+    private void Start()
+    {
+        global = GetComponent<Light2D>();
+    }
+
+    public void Update()
+    {
+        if (fadingPlay)
+        {
+            global.intensity -= Time.deltaTime * fadeSpeed;
+            if(global.intensity <= 0)
+            {
+                fadingPlay = false;
+                GameObject.Find("SceneManager").GetComponent<MainMenuButtons>().LoadNextScene();
+            }
+        }
+
+        if (fadingQuit)
+        {
+            global.intensity -= Time.deltaTime * fadeSpeed;
+            if (global.intensity <= 0)
+            {
+                fadingQuit = false;
+                GameObject.Find("SceneManager").GetComponent<MainMenuButtons>().AppQuit();
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Eric/Scripts/MainMenuButtons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+
+    public void PlayButtonPress()
+    {
+        GameObject.Find("Global light").GetComponent<LightFade>().fadingPlay = true;
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitButtonPress()
+    {
+        GameObject.Find("Global light").GetComponent<LightFade>().fadingQuit = true;
+    }
+
+
+    public void AppQuit()
+    {
+    Application.Quit();
+    Debug.Log("Game Quit To Desktop");
+    }
+
+}
