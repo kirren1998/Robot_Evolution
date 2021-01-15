8d57107395a1d2bfb1d64b1abbc025cc0af31ba3
Oscar Jakobsson <oscar.jakobsson@gmail.com>
2021-01-15 16:57:17 +0100

    Boss and Arm Jump

---
+++ Assets/Arm.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arm : MonoBehaviour
+{
+    [Range (1, 5)][SerializeField] float butt;
+    [SerializeField] Animator arm;
+    public void Jumpstart()
+    {
+        Invoke("Jumpfinish", butt);
+    }
+    public void Jumpfinish()
+    {
+        arm.SetTrigger("Arm Jump");
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            arm.SetTrigger("Start");
+        }
+    }
+}
